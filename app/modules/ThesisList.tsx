0d59336ca15d18eea235c41c0f6cb2d993b9f470
annagaom <122282732+annagaom@users.noreply.github.com>
2026-05-28 19:35:58 +0300

    Change the year range input field to a dropdown

--- app/modules/ThesisList.tsx
+++ app/modules/ThesisList.tsx
@@ -6,7 +6,7 @@ import { Hoverable } from "react-native-web-hover";
 import { config } from "../config";
 import { DownloadCsv } from "../components/DownloadCsv";
 import { SelectBox } from "../components/UI/SelectBox";
-import { DropdownBox } from "../components/UI/DropdownBox";
+import { UniversitySelectDropdown, YearRangeDropdown } from "../components/UI/DropdownBox";
 
 const uniCodes = [
   {"uni": "All", "code": "all"},
@@ -466,7 +466,7 @@ export default function ThesisList() {
               setDropdownOpen={setDropdownOpen}
             />
 
-            <DropdownBox
+            <UniversitySelectDropdown
               dropdownOpen={dropdownOpen}
               hoveredGroup={hoveredGroup}
               setHoveredGroup={setHoveredGroup}
@@ -485,21 +485,13 @@ export default function ThesisList() {
             onChangeText={text => setSelectedAuthor(text)}
           />
 
-          <TextInput
-            style={styles.inputField}
-            placeholder="From Year..."
-            placeholderTextColor="#999"
-            keyboardType="numeric"
-            onChangeText={text => setStartYear(text)}
+          <YearRangeDropdown
+            startYear={startYear}
+            endYear={endYear}
+            setStartYear={setStartYear}
+            setEndYear={setEndYear}
           />
 
-          <TextInput
-            style={styles.inputField}
-            placeholder="To Year..."
-            placeholderTextColor="#999"
-            keyboardType="numeric"
-            onChangeText={text => setEndYear(text)}
-          />
         </View>
 
         <View style={styles.buttonRow}>
