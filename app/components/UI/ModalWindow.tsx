02780b8b3e06866447b01be45b50a24fef3bcd53
annagaom <122282732+annagaom@users.noreply.github.com>
2026-05-31 12:52:42 +0300

    Replace summary page with modal view"

---
+++ app/components/UI/ModalWindow.tsx
@@ -0,0 +1,67 @@
+import { Modal, View, TouchableOpacity, Text, StyleSheet } from "react-native";
+
+interface PopWindowProps {
+  visible: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+export default function ModalWindow({
+  visible,
+  onClose,
+  children,
+}: PopWindowProps) {
+  return (
+    <Modal
+      visible={visible}
+      transparent
+      animationType="fade"
+      onRequestClose={onClose}
+    >
+      <View style={styles.overlay}>
+        <View style={styles.window}>
+          <TouchableOpacity
+            style={styles.closeButton}
+            onPress={onClose}
+          >
+            <Text style={styles.closeText}>×</Text>
+          </TouchableOpacity>
+
+          {children}
+        </View>
+      </View>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+  overlay: {
+    flex: 1,
+    backgroundColor: "rgba(0,0,0,0.4)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  window: {
+    width: "80%",
+    height: "90%",
+    backgroundColor: "#fff",
+    borderRadius: 15,
+    padding: 25,
+  },
+  closeButton: {
+    position: "absolute",
+    right: 20,
+    top: 20,
+    zIndex: 100,
+    alignSelf: "center",
+    backgroundColor: "red",
+    paddingVertical: 1,
+    paddingHorizontal: 8,
+    borderRadius: 8
+  },
+  closeText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+});
